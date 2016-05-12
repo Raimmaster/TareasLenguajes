2466f11d97bbff8d96a9925c4c16a5eb3fb85728
Raim <rai.interiano@gmail.com>
2016-05-11 19:45:22 -0600

    Something

--- Python/Client-Server/python_server.py
+++ Python/Client-Server/python_server.py
@@ -86,6 +86,8 @@ while option_received != 3: #3 salir
 						loggedUser.getCurrentDirName()
 					elif(option_received == 9) #exit
 						print('Logging user off...')	
+						mensaje_enviar = 'Log Off:'
+						client_socket.sendall(mensaje_enviar.encode('ascii'))								
 	finally:			
 		print('Ha acabado.')
 
